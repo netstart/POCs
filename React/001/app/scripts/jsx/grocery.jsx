a7fc8c78b0a16c707d5335fe9567ee633423588f
unknown <claytonpassos@clayton-passos.elotech.localnet>
2015-04-08 08:51:44 -0300

    update

--- React/001/app/scripts/jsx/grocery.jsx
+++ React/001/app/scripts/jsx/grocery.jsx
@@ -7,37 +7,34 @@ var myItems = [
 
 var GroceryItem = React.createClass({
   render: function() {
-  return (
-    <li className='grocery-list-item'>
-  <span className='name'>{this.props.name}</span>
-<span className='price'>
-R${this.props.price}
-</span>
-</li>
-);
-}
+      return (
+        <li className='grocery-list-item'>
+          <span className='name'>{this.props.name}</span>
+          <span className='price'>R${this.props.price}</span>
+        </li>
+    );
+  }
 });
 
 var GroceryList = React.createClass({
     render: function() {
       var items = this.props.items.map(function(item, i) {
         return (
-          <GroceryItem name={item.name} price={item.price} />
-);
-});
+            <GroceryItem name={item.name} price={item.price} />
+        );
+      });
 
-return (
-  <div className='grocery-list'>
-<h1 className='title'>{this.props.name}</h1>
-<ul className='grocery-list'>
-{items}
-</ul>
-</div>
-);
-}
+      return (
+          <div className='grocery-list'>
+          <h1 className='title'>{this.props.name}</h1>
+            <ul className='grocery-list'>
+             {items}
+            </ul>
+          </div>
+      );
+    }
 });
 
 React.render(
-<GroceryList name='My Grocery List' items={myItems} />,
-  document.querySelector('#content')
+  <GroceryList name='My Grocery List' items={myItems} />, document.querySelector('#content')
 );
