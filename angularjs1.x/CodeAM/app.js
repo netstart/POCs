a64ecffad6fd88e9b5e717a0fea1e9256fa5b89a
Clayton K. N. Passos <netstart@gmail.com>
2015-07-16 23:20:16 -0300

    2, and 3 class

--- angularjs1.x/CodeAM/app.js
+++ angularjs1.x/CodeAM/app.js
@@ -1,1 +1,9 @@
 var codeAmApp = angular.module('codeAmApp', []);
+
+codeAmApp.controller("JoaoCtrl", ['$scope', function($scope){
+	$scope.name = 'João';
+}]);
+
+codeAmApp.controller("MariaCtrl", ['$scope', function($scope){
+	$scope.name = 'Maria';
+}]);
