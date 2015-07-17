802b192712c90b5c77b1398d7c400951fddb9812
Clayton K. N. Passos <netstart@gmail.com>
2015-07-16 23:37:07 -0300

    add button with function in controller

--- angularjs1.x/CodeAM/app.js
+++ angularjs1.x/CodeAM/app.js
@@ -2,6 +2,12 @@ var codeAmApp = angular.module('codeAmApp', []);
 
 codeAmApp.controller("JoaoCtrl", ['$scope', function($scope){
 	$scope.name = 'João';
+
+	$scope.habilitaNome = function(){
+		$scope.name = "Clayton K. N. Passos"
+	}
+
+
 }]);
 
 codeAmApp.controller("MariaCtrl", ['$scope', function($scope){
