32c76d677e4ebccaa1f49d1c8eba557293e0c0c3
Clayton K. N. Passos <netstart@gmail.com>
2015-12-21 01:23:17 -0200

    add angular js

---
+++ MEAN/DiegoLopes-001/03/public/javascript/controllers/posts.js
@@ -0,0 +1,38 @@
+var superhero = angular.module("superhero", []);
+
+superhero.controller("PostCtrl", ['$scope', function($scope){
+	var posts = [
+		{
+			title: 'Publicação 1',
+			content: 'Conteúdo 1',
+			tags: [],
+			likes: 10
+		},
+		{
+			title: 'Publicação 2',
+			content: 'Conteúdo 3',
+			tags: [],
+			likes: 9
+		},
+		{
+			title: 'Publicação 2',
+			content: 'Conteúdo 3',
+			tags: [],
+			likes: 11
+		}
+	];
+
+	$scope.posts = posts;
+
+
+	//Funções
+
+	$scope.addLike = function(post){
+		post.likes += 1;
+	};
+
+	$scope.removeLike = function(post) {
+		post.likes -= 1;
+	};
+
+}]);
