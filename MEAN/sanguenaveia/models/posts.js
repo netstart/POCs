fe0b622c5bc5300001006f87232348083f7aa657
Clayton K. N. Passos <netstart@gmail.com>
2016-01-10 18:06:43 -0200

    .

--- MEAN/sanguenaveia/models/posts.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var postSchema = new Schema({ title: String, content: String, tags: [] });
-
-var Post = mongoose.model('posts', postSchema);
-
-module.exports = Post;
--- MEAN/sanguenaveia/models/users.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var userSchema = new Schema({ name: String, user: String});
-
-var User = mongoose.model('users', userSchema);
-
-module.exports = User;
