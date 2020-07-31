746ccf25b33ae621545e585fa53715fe16cbf559
Clayton <netstart@gmail.com>
2020-07-31 11:18:15 -0300

    comentário

--- redis/04-redis-spring-actuator/src/main/java/com/github/netstart/redis/MeterConfig.java
+++ redis/04-redis-spring-actuator/src/main/java/com/github/netstart/redis/MeterConfig.java
@@ -33,7 +33,13 @@ public class MeterConfig {
 		// Assim não atualiza em tempo de execução
 		// registry.gauge("protection.scan.key.total.query.all.x", cacheService.totalConsultasDaAplicacaoToda());
 		 
-		// Assim, ao chamar /actuator/prometheus ele irá buscar a informação no cacheService e atualizar o valor
+		/* 
+		 * Assim, ao chamar /actuator/prometheus ele irá buscar a informação no cacheService e atualizar o valor
+		 * 
+		 * Para não ir sempre no Redis, e acabar se tornando lento, 
+		 * é legal usar @Cacheble ou outro mecanismo de cache de armazenamento local (armazenamento em memória da própria aplicação), 
+		 * que expira de tempos em tempos.
+		 */
 		return Gauge
 			.builder("protection.scan.key.total.query.all", ()-> cacheService.totalConsultasDaAplicacaoToda())
 			.description("Total of key query on the window time: ")
