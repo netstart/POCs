85cdb7d8189a5106ee3f2277559188bef3785efd
cknp <clayton.passos@matera.com>
2021-02-26 15:02:09 -0300

    first commit

---
+++ mTLS/01-mTLS/mtls-client/src/main/java/com/github/mtlsclient/MtlsClientApplication.java
@@ -0,0 +1,13 @@
+package com.github.mtlsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtlsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtlsClientApplication.class, args);
+	}
+
+}
---
+++ mTLS/01-mTLS/mtls-client/src/test/java/com/github/mtlsclient/MtlsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mtlsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtlsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
